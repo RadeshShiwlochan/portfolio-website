99a818fad203704f7dde4e9ac1613b90ada21f05
radeshshiwlochan <radesh0430@gmail.com>
2018-01-06 14:18:13 -0500

    adds Open Sans font, partials for menu

---
+++ routes/routes.js
@@ -0,0 +1,24 @@
+const express = require( 'express' );
+const router = express.Router();
+
+router.get( '/', ( req, res ) => {
+    res.render( 'home' );
+});
+
+router.get( '/projects', ( req, res ) => {
+	res.render( 'projects' );
+});
+
+router.get( '/about-me', ( req, res ) => {
+	res.render( 'about-me' );
+});
+
+router.get( '/skills', ( req, res ) => {
+
+});
+
+router.get( '/contact', ( req, res ) => {
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require( 'express' );
 const app     = express();
 const port    = process.env.PORT || 3000;
 
+const routes = require( './routes/routes.js' );
+
 app.use(express.static( 'public' ) );
 app.set( 'view engine', 'ejs' );
 
-app.get( '/', ( req, res ) => {
-    res.render( 'home' );
-});
+app.use( '/', routes );
 
 app.listen( port, () => {
 	console.log( `Listening on port:{port}` );
